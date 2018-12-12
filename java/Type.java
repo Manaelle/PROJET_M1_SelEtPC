19e6af46e9c9e39846de5381019ca1bb0bb27405
hirouka <nadia.benmoussa@etu.univ-grenoble-alpes.fr>
2018-12-12 14:35:42 +0100

    type

--- java/Type.java
+++ java/Type.java
@@ -6,19 +6,63 @@ abstract class Type {
     
 }
 
-class TUnit extends Type { }
+class TUnit extends Type { 
+    
+    public String toString(){
+	return "unit";
+	}
+    
+    public boolean equals(Object o){
+        return(o instanceof TUnit );
+    }
+}
 
-class TBool extends Type { }
+class TBool extends Type {
+    
+    public String toString(){
+	return "bool";
+    }
+    public boolean equals(Object o){
+        return(o instanceof TBool );
+    }
+}
 
-class TInt extends Type { }
+class TInt extends Type { 
+    
+    public String toString(){
+	return "int";
+    }
+    public boolean equals(Object o){
+        return(o instanceof TInt );
+    }
+        
+}
 
-class TFloat extends Type { }
+class TFloat extends Type { 
+    
+    public String toString(){
+	return "float";
+    }
+    public boolean equals(Object o){
+        return(o instanceof TFloat );
+    }
+}
 
-class TFun extends Type { }
+class TFun extends Type {}
 
 class TTuple extends Type { }
 
-class TArray extends Type { }
+class TArray extends Type {
+    public Type type;
+    
+    public TArray(Type type){
+        this.type = type; 
+        
+    }
+    public String  ToString(){
+        return "array of("+this.type+")";
+    }
+}
 
 class TVar extends Type {
     String v;
