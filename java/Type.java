f0c2a193ea279eb5e1756aea08e90d8a34ea8c1b
hirouka <nadia.benmoussa@etu.univ-grenoble-alpes.fr>
2018-12-12 15:50:14 +0100

    type +

--- java/Type.java
+++ java/Type.java
@@ -1,5 +1,12 @@
+
+import java.util.List;
+
 abstract class Type {
     private static int x = 0;
+    /**
+     * generer le type 
+     * @return le Type généré 
+     */
     static Type gen() {
         return new TVar("?" + x++);
     }
@@ -48,9 +55,82 @@ class TFloat extends Type {
     }
 }
 
-class TFun extends Type {}
+class TFun extends Type {
+    public List<Type> arguments;
+    public Type typeRetour;
+    
+    public TFun(List<Type> arguments ,Type typeRetour){
+        this.arguments= arguments;
+        this.typeRetour = typeRetour;
+        
+    }
+  /*  
+    public TFun(List<Type> arguments){
+        this.arguments = arguments ; 
+    }
+    */
+    
+    public String ToString(Object o){
+      String s = "(fun : ";
+      int i = 0 ;
+      while(i<arguments.size()){
+          s= s + arguments.get(i) + "->" ;
+          i++;
+      }
+      s = s + ")";
+      return s;  
+    }
+    
+    public boolean equals(Object o){
+        if(!(o instanceof TFun)){
+            return false ; 
+            
+        }else{
+            return (this.typeRetour.equals(((TFun)o).typeRetour)) && (this.arguments.equals(((TFun)o).arguments)) ;
+        }
+        
+        
+    }
+    
+    
+}
 
-class TTuple extends Type { }
+class TTuple extends Type { 
+    public List<Type> list; 
+    /**
+     * constructeur de List de type 
+     * @param list 
+     */
+    public TTuple(List<Type> list){
+        this.list = list;
+    }
+    /**
+     *exemple : let person = ("larry", 47, 165, ’M’);;
+     *val person : string * int * int * char = ("larry", 47, 165, ’M’)
+     * @return 
+     */
+    public String ToString(){
+        int i =1;
+        String s = "(";
+        s = s + list.get(0);
+        while(i<list.size()){
+            s= s + "* " + list.get(i); 
+            i++;
+        }
+        s = s + ")";
+        return s;
+       
+    }
+    
+   public boolean equals(Object o){
+       if(!(o instanceof TTuple)){
+           return false; 
+       }else{
+           return this.list.equals(((TTuple)o).list);   
+       }
+    }
+    
+}
 
 class TArray extends Type {
     public Type type;
@@ -61,6 +141,14 @@ class TArray extends Type {
     }
     public String  ToString(){
         return "array of("+this.type+")";
+    }
+    
+    public boolean equals(Object o){
+        if(!(o instanceof TArray )){
+            return false ;
+        }else {
+            return this.type.equals(((TArray)o).type);
+        }
     }
 }
 
